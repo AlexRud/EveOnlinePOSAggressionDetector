67e25f45d92144ff99402dbfa57e649e60565079
AlexRud <alex.rudall@hotmail.co.uk>
2015-10-01 15:19:13 +0100

    API requests.

--- src/whudushutpos/WhuDuShutPosGUI.java
+++ src/whudushutpos/WhuDuShutPosGUI.java
@@ -21,11 +21,14 @@ import java.util.logging.Logger;
  */
 public class WhuDuShutPosGUI extends javax.swing.JFrame {
 
+    Timer timer = new Timer();
+
     /**
      * Creates new form WhuDuShutPosGUI
      */
     public WhuDuShutPosGUI() {
         initComponents();
+        StopButton.setVisible(false);
     }
 
     //https://api.eveonline.com/char/Notifications.xml.aspx?keyID=4474332&vCode=5MuDcndf6vTeYbBqhAZc7PdWGEH8XI6HfoenntMRoe50LY8mxEdWAj2uSt4mqzUR&characterID=95477198 notifications request.
@@ -34,90 +37,62 @@ public class WhuDuShutPosGUI extends javax.swing.JFrame {
     //keyID, vCode, characterID, notification id(s) from previous request.
     //https://api.eveonline.com/eve/CharacterInfo.xml.aspx?characterid=94818624 character info request.
     //Character name, corp name, alliance name, shield hp, time, system?
-    public void something() {
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
+    //public void getNotifications() {
+    TimerTask task = new TimerTask() {
             @Override
             public void run() {
-
-            }
-        }, 2 * 60 * 1000, 2 * 60 * 1000);
-    }
-
-    public void getNotifications() {
-        try {
-            URL url = new URL(weblink.getText());
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()))) {
-                String inputLine;
-                while ((inputLine = in.readLine()) != null) {
-                    if (inputLine.contains("typeID=\"75\"")) {
-                        String s = inputLine.substring(inputLine.indexOf("=") + 2, inputLine.indexOf("typeID") - 2);
-                        String edit = weblink.getText().replace("Notifications", "NotificationTexts");
-                        URL url1 = new URL(edit + "&IDs=" + s);
-                        try (BufferedReader in1 = new BufferedReader(new InputStreamReader(url1.openStream()))) {
-                            String inputLine2;
-                            while ((inputLine2 = in1.readLine()) != null) {
-                                if (inputLine2.contains("aggressorID")) {
-                                    String s1 = inputLine2.substring(inputLine2.lastIndexOf(":") + 2);
-                                    URL url2 = new URL("https://api.eveonline.com/eve/CharacterInfo.xml.aspx?characterid=" + s1);
-                                    try (BufferedReader in2 = new BufferedReader(new InputStreamReader(url2.openStream()))) {
-                                        String inputLine3;
-                                        while ((inputLine3 = in2.readLine()) != null) {
-                                            if (inputLine3 != null && inputLine3.contains("characterName")) {
-                                                String s2 = inputLine3.substring(inputLine3.indexOf(">") + 1, inputLine3.indexOf("</"));
-                                                System.out.println(s2);
-                                                outputPlace.append(s2);
-                                            }
-                                            if (inputLine3 != null && inputLine3.contains("<corporation>")) {
-                                                String s3 = inputLine3.substring(inputLine3.indexOf(">") + 1, inputLine3.indexOf("</"));
-                                                System.out.println(s3);
-                                                outputPlace.append("\n" +s3);
-                                            }
-                                            if (inputLine3 != null && inputLine3.contains("<alliance>")) {
-                                                String s4 = inputLine3.substring(inputLine3.indexOf(">") + 1, inputLine3.indexOf("</"));
-                                                System.out.println(s4);
-                                                outputPlace.append("\n" + s4);
+                try {
+                    URL url = new URL(weblink.getText());
+                    try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()))) {
+                        String inputLine;
+                        while ((inputLine = in.readLine()) != null) {
+                            if (inputLine.contains("typeID=\"75\"")) {
+                                String s = inputLine.substring(inputLine.indexOf("=") + 2, inputLine.indexOf("typeID") - 2);
+                                String edit = weblink.getText().replace("Notifications", "NotificationTexts");
+                                URL url1 = new URL(edit + "&IDs=" + s);
+                                try (BufferedReader in1 = new BufferedReader(new InputStreamReader(url1.openStream()))) {
+                                    String inputLine2;
+                                    while ((inputLine2 = in1.readLine()) != null) {
+                                        if (inputLine2.contains("aggressorID")) {
+                                            String s1 = inputLine2.substring(inputLine2.lastIndexOf(":") + 2);
+                                            URL url2 = new URL("https://api.eveonline.com/eve/CharacterInfo.xml.aspx?characterid=" + s1);
+                                            try (BufferedReader in2 = new BufferedReader(new InputStreamReader(url2.openStream()))) {
+                                                String inputLine3;
+                                                while ((inputLine3 = in2.readLine()) != null) {
+                                                    if (inputLine3 != null && inputLine3.contains("characterName")) {
+                                                        String s2 = inputLine3.substring(inputLine3.indexOf(">") + 1, inputLine3.indexOf("</"));
+                                                        System.out.println(s2);
+                                                        outputPlace.append(s2);
+                                                    }
+                                                    if (inputLine3 != null && inputLine3.contains("<corporation>")) {
+                                                        String s3 = inputLine3.substring(inputLine3.indexOf(">") + 1, inputLine3.indexOf("</"));
+                                                        System.out.println(s3);
+                                                        outputPlace.append("\n" + s3);
+                                                    }
+                                                    if (inputLine3 != null && inputLine3.contains("<alliance>")) {
+                                                        String s4 = inputLine3.substring(inputLine3.indexOf(">") + 1, inputLine3.indexOf("</"));
+                                                        System.out.println(s4);
+                                                        outputPlace.append("\n" + s4);
+                                                    }                                                    
+                                                }
+                                                outputPlace.append("\n-----------------------\n");
                                             }
                                         }
                                     }
                                 }
                             }
                         }
+                    } catch (IOException ex) {
+                        Logger.getLogger(WhuDuShutPosGUI.class.getName()).log(Level.SEVERE, null, ex);
                     }
+
+                } catch (MalformedURLException ex) {
+                    Logger.getLogger(WhuDuShutPosGUI.class.getName()).log(Level.SEVERE, null, ex);
                 }
-            } catch (IOException ex) {
-                Logger.getLogger(WhuDuShutPosGUI.class.getName()).log(Level.SEVERE, null, ex);
+
             }
-
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(WhuDuShutPosGUI.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public void doTheThing() {
-        try {
-            URL URL = new URL(weblink.getText());
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(URL.openStream()))) {
-                String inputLine;
-                while ((inputLine = in.readLine()) != null) {
-                    if (inputLine.contains("characterName")) {
-                        String s = inputLine.substring(inputLine.indexOf(">") + 1, inputLine.indexOf("</"));
-                        System.out.println(s);
-                    }
-                    if (inputLine.contains("corporation")) {
-
-                    }
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(WhuDuShutPosGUI.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(WhuDuShutPosGUI.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    
+        };
+    //}
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -132,6 +107,7 @@ public class WhuDuShutPosGUI extends javax.swing.JFrame {
         webbuttan = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         outputPlace = new javax.swing.JTextArea();
+        StopButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -148,6 +124,13 @@ public class WhuDuShutPosGUI extends javax.swing.JFrame {
         outputPlace.setRows(5);
         jScrollPane1.setViewportView(outputPlace);
 
+        StopButton.setText("Stop");
+        StopButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                StopButtonMouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -157,9 +140,11 @@ public class WhuDuShutPosGUI extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(weblink)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 290, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
-                .addComponent(webbuttan)
-                .addContainerGap())
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(webbuttan, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(StopButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -169,7 +154,11 @@ public class WhuDuShutPosGUI extends javax.swing.JFrame {
                     .addComponent(weblink, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(webbuttan))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 249, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 249, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(StopButton)
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
 
@@ -177,8 +166,16 @@ public class WhuDuShutPosGUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void webbuttanMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_webbuttanMouseClicked
-        getNotifications();
+        timer.scheduleAtFixedRate(task, 0, 1000*60*30);
+        webbuttan.setVisible(false);
+        StopButton.setVisible(true);
     }//GEN-LAST:event_webbuttanMouseClicked
+
+    private void StopButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_StopButtonMouseClicked
+        task.cancel();
+        webbuttan.setVisible(true);
+        StopButton.setVisible(false);
+    }//GEN-LAST:event_StopButtonMouseClicked
 
     /**
      * @param args the command line arguments
@@ -216,6 +213,7 @@ public class WhuDuShutPosGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton StopButton;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea outputPlace;
     private javax.swing.JButton webbuttan;
