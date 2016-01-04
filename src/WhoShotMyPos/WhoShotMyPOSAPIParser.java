96eb0b680dddb734e2af77a90e319d1267e3b180
AlexRud <alex.rudall@hotmail.co.uk>
2016-01-04 23:30:05 +0000

    Rename Commit

--- src/WhoShotMyPos/WhoShotMyPOSAPIParser.java
+++ src/WhoShotMyPos/WhoShotMyPOSAPIParser.java
@@ -6,11 +6,18 @@
 package WhoShotMyPos;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Timer;
@@ -25,18 +32,20 @@ import net.gpedro.integrations.slack.SlackMessage;
  * @author Alex
  */
 public class WhoShotMyPOSAPIParser extends javax.swing.JFrame {
-
-    Timer timer = new Timer();
+    
+    Timer timer;
     SlackApi api;
-    HashMap<Integer, String> systemNames  = new HashMap();
+    HashMap<Integer, String> systemNames = new HashMap();
+    ArrayList notificationID = new ArrayList();
+
     /**
      * Creates new form WhuDuShutPosGUI
      */
     public WhoShotMyPOSAPIParser() {
         initComponents();
-        StopButton.setVisible(false);
         loadSystemName();
-        api = new SlackApi("https://hooks.slack.com/services/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");       
+        loadNotificationIDs();
+        api = new SlackApi("https://hooks.slack.com/services/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");        
     }
 
     //https://api.eveonline.com/char/Notifications.xml.aspx?keyID=4474332&vCode=5MuDcndf6vTeYbBqhAZc7PdWGEH8XI6HfoenntMRoe50LY8mxEdWAj2uSt4mqzUR&characterID=95477198 notifications request.
@@ -49,27 +58,29 @@ public class WhoShotMyPOSAPIParser extends javax.swing.JFrame {
     //https://api.eveonline.com/char/Notifications.xml.aspx?keyID=455684&vCode=oAPDhYH9pc063j5GWszkvwvpPwC3fPD6FX515Q1JAl79RXoBhy9GInhMNth2Dutu&characterID=151627406
     //public void getNotifications() {
     TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                String timeDate = null;
-                String character = null;
-                String corp = null;
-                String alliance = null;
-                String shieldHP = null;
-                String system = null;
-                try {                                     
-                    URL url = new URL(weblink.getText());
-                    try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()))) {
-                        String inputLine;
-                        while ((inputLine = in.readLine()) != null) {
-                            if (inputLine.contains("typeID=\"75\"")) {                                
-                                String s = inputLine.substring(inputLine.indexOf("=") + 2, inputLine.indexOf("typeID") - 2);
+        @Override
+        public void run() {
+            String timeDate = null;
+            String character = null;
+            String corp = null;
+            String alliance = null;
+            String shieldHP = null;
+            String system = null;
+            try {                
+                URL url = new URL(weblink.getText());
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()))) {
+                    String inputLine;
+                    while ((inputLine = in.readLine()) != null) {
+                        if (inputLine.contains("typeID=\"75\"")) {                            
+                            String s = inputLine.substring(inputLine.indexOf("=") + 2, inputLine.indexOf("typeID") - 2);
+                            if (!notificationID.contains(s)) {                                
+                                notificationID.add(s);
                                 timeDate = inputLine.substring(inputLine.indexOf("sentDate=") + 10, inputLine.indexOf("sentDate=") + 29);
                                 String edit = weblink.getText().replace("Notifications", "NotificationTexts");
                                 URL url1 = new URL(edit + "&IDs=" + s);
                                 try (BufferedReader in1 = new BufferedReader(new InputStreamReader(url1.openStream()))) {
                                     String inputLine2;
-                                    while ((inputLine2 = in1.readLine()) != null) {       
+                                    while ((inputLine2 = in1.readLine()) != null) {                                        
                                         if (inputLine2.contains("aggressorID")) {
                                             String s1 = inputLine2.substring(inputLine2.lastIndexOf(":") + 2);
                                             URL url2 = new URL("https://api.eveonline.com/eve/CharacterInfo.xml.aspx?characterid=" + s1);
@@ -83,36 +94,44 @@ public class WhoShotMyPOSAPIParser extends javax.swing.JFrame {
                                                         corp = inputLine3.substring(inputLine3.indexOf(">") + 1, inputLine3.indexOf("</"));                                                        
                                                     }
                                                     if (inputLine3 != null && inputLine3.contains("<alliance>")) {
-                                                        alliance = inputLine3.substring(inputLine3.indexOf(">") + 1, inputLine3.indexOf("</"));                                                                                                          }                                                    
-                                                }                                              
+                                                        alliance = inputLine3.substring(inputLine3.indexOf(">") + 1, inputLine3.indexOf("</"));
+                                                    }                                                    
+                                                }                                                
                                             }
                                         }
-                                        if(inputLine2.contains("solarSystemID")){
+                                        if (inputLine2.contains("solarSystemID")) {
                                             String solarSystem = inputLine2.substring(inputLine2.lastIndexOf(":") + 2);
                                             int id = Integer.parseInt(solarSystem);
                                             system = systemNames.get(id);
                                         }
-                                        if(inputLine2.contains("shieldValue")){
-                                            shieldHP = inputLine2.substring(inputLine2.lastIndexOf(":") + 2);                                            
+                                        if (inputLine2.contains("shieldValue")) {
+                                            shieldHP = inputLine2.substring(inputLine2.lastIndexOf(":") + 4, inputLine2.lastIndexOf(":") + 6) +"%";  
+                                            
                                         }                                        
                                     }
-                                    outputPlace.append(timeDate + "\n" + character + " \n" + corp + "\n" + alliance + "\n" + shieldHP + "\n" + system + "\n --------------------");
-                                    String output = timeDate + "\n" + character + " \n" + corp + "\n" + alliance + "\n" + shieldHP + "\n" + system + "\n -----------------------";
+                                    outputPlace.append("Time: " + timeDate + "\nAttacker: " + character + " \nAttacker Corporation: " + corp + "\nAttacker Alliance: " + alliance + "\nPOS Shield HP: " + shieldHP 
+                                            + "\nSystem: " + system + "\n---------------------");
+                                    String output = "Time: " + timeDate + "\nAttacker: " + character + " \nAttacker Corporation: " + corp + "\nAttacker Alliance: " + alliance + "\nPOS Shield HP: " + shieldHP 
+                                            + "\nSystem: " + system + "\n---------------------";
+                                    saveNotificationIDs();
                                     api.call(new SlackMessage("#posbotspam" , output));
                                 }
+                            } else {
+                                notificationID.add(s);
                             }
-                            
-                        }                        
-                    } catch (IOException ex) {
-                        Logger.getLogger(WhoShotMyPOSAPIParser.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-
-                } catch (MalformedURLException ex) {
+                        }
+                        
+                    }                    
+                } catch (IOException ex) {
                     Logger.getLogger(WhoShotMyPOSAPIParser.class.getName()).log(Level.SEVERE, null, ex);
                 }
-
+                
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(WhoShotMyPOSAPIParser.class.getName()).log(Level.SEVERE, null, ex);
             }
-        };
+            
+        }
+    };
     //}
 
     /**
@@ -129,9 +148,12 @@ public class WhoShotMyPOSAPIParser extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         outputPlace = new javax.swing.JTextArea();
         StopButton = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        weblink.setAutoscrolls(false);
+        weblink.setMaximumSize(new java.awt.Dimension(290, 21));
+        weblink.setMinimumSize(new java.awt.Dimension(290, 21));
 
         webbuttan.setText("Do Work");
         webbuttan.setFocusable(false);
@@ -146,17 +168,10 @@ public class WhoShotMyPOSAPIParser extends javax.swing.JFrame {
         outputPlace.setRows(5);
         jScrollPane1.setViewportView(outputPlace);
 
-        StopButton.setText("Stop");
+        StopButton.setText("Close");
         StopButton.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 StopButtonMouseClicked(evt);
-            }
-        });
-
-        jButton1.setText("Testies");
-        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButton1MouseClicked(evt);
             }
         });
 
@@ -167,54 +182,42 @@ public class WhoShotMyPOSAPIParser extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(weblink)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 290, Short.MAX_VALUE))
+                    .addComponent(jScrollPane1)
+                    .addComponent(weblink, javax.swing.GroupLayout.PREFERRED_SIZE, 290, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(webbuttan, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(StopButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addComponent(jButton1))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(webbuttan, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(StopButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(weblink, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(webbuttan))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(webbuttan)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(weblink, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(2, 2, 2)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 249, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(18, 18, 18)
-                        .addComponent(StopButton)
-                        .addGap(18, 18, 18)
-                        .addComponent(jButton1)
-                        .addGap(0, 0, Short.MAX_VALUE)))
-                .addContainerGap())
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 245, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(StopButton))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void webbuttanMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_webbuttanMouseClicked
-        timer.scheduleAtFixedRate(task, 0, 1000*60*15);
-        webbuttan.setVisible(false);
-        StopButton.setVisible(true);
+        timer = new Timer();
+        timer.scheduleAtFixedRate(task, 0, 1000 * 60 * 15);
     }//GEN-LAST:event_webbuttanMouseClicked
 
     private void StopButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_StopButtonMouseClicked
-        task.cancel();
-        webbuttan.setVisible(true);
-        StopButton.setVisible(false);
+        dispose();
     }//GEN-LAST:event_StopButtonMouseClicked
-
-    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
-        api.call(new SlackMessage("#posbotspam" , "my message"));
-    }//GEN-LAST:event_jButton1MouseClicked
-
+    
     private void loadSystemName() {
         BufferedReader br;
         try {
@@ -232,6 +235,43 @@ public class WhoShotMyPOSAPIParser extends javax.swing.JFrame {
         }
     }
     
+    private void saveNotificationIDs() {
+        try {
+            File f = new File("notificationIDs.data");
+            FileOutputStream f_out = new FileOutputStream(f);
+            ObjectOutputStream obj_out;
+            obj_out = new ObjectOutputStream(f_out);
+            obj_out.writeObject(notificationID);
+            System.out.println(notificationID);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+    
+    private void loadNotificationIDs() {
+        FileInputStream f_in1 = null;
+        File f1 = new File("notificationIDs.data");
+        
+        if (f1.exists()) {
+            try {
+                f_in1 = new FileInputStream(f1);
+                ObjectInputStream obj_in1 = new ObjectInputStream(f_in1);
+                notificationID = (ArrayList) obj_in1.readObject();                
+                System.out.println(notificationID);
+                
+                obj_in1.close();
+                f_in1.close();
+                
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(WhoShotMyPOSAPIParser.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(WhoShotMyPOSAPIParser.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(WhoShotMyPOSAPIParser.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
@@ -270,7 +310,6 @@ public class WhoShotMyPOSAPIParser extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton StopButton;
-    private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea outputPlace;
     private javax.swing.JButton webbuttan;
